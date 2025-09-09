98d9ab916dbd9eae4bc34437dab022bbc1268ccb
Alexey <4681325+qqrm@users.noreply.github.com>
2025-09-09 22:46:05 +0300

    ci: avoid hanging in examples

--- run_examples.sh
+++ run_examples.sh
@@ -8,9 +8,9 @@ printf '== network-build ==\n'
     WARDEN_OFFLINE=1 cargo run -p cargo-warden -- build
 )
 
+# Build spawn-bash example
 printf '\n== spawn-bash ==\n'
 (
     cd examples/spawn-bash
-    cargo run -p cargo-warden -- run -- cargo run
+    cargo build
 )
-
