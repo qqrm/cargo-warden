1f1fda2fef5547a19ca563e21b104b3ec95bf9a9
Alexey <4681325+qqrm@users.noreply.github.com>
2025-09-10 01:40:59 +0300

    ci: build network example in scripts

--- run_examples.sh
+++ run_examples.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Run network-build example
+# Build network-build example
 printf '== network-build ==\n'
 (
     cd examples/network-build
-    WARDEN_OFFLINE=1 cargo run -p cargo-warden -- build
+    cargo build
 )
 
 # Build spawn-bash example
